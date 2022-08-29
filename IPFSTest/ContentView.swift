8c5eee16e6aee5934933333c6254c8ea16c6e24f
Bryan Albert <bryan2j@gmail.com>
2022-08-28 23:15:58 -0600

    Initial Commit

---
+++ IPFSTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IPFSTest
+//
+//  Created by Bryan Albert on 8/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IPFSTest/IPFSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPFSTestApp.swift
+//  IPFSTest
+//
+//  Created by Bryan Albert on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct IPFSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
